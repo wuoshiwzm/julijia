b31254fd06d694d3cf691218b855c62c9cd40c9c
wuoshiwzm <wuoshiwzm@hotmail.com>
2016-12-18 19:18:29 +0800

    12-18

--- app/controllers/member/CartMemberController.php
+++ app/controllers/member/CartMemberController.php
@@ -32,7 +32,7 @@ class CartMemberController extends CommonController
     }
 
     /**
-     *
+     *购物车首页
      */
     function index()
     {
@@ -43,8 +43,50 @@ class CartMemberController extends CommonController
         $data = $this->carts->paginate($setPage);
         $carts = $this->carts->get();
 
-
         return $this->view('member.cart.index', compact('carts', 'data', 'set'));
+    }
+
+
+    function checkDiscount()
+    {
+
+        //获取对应 quoteSn数组
+        $quoteSnArr = Input::get('quoteSns');
+
+        //对应总的重量
+        $weight = 0;
+        //对应总的价格
+        $money = 0;
+        //对应对应总的折扣价格  减少的价格
+        $discount = 0;
+
+
+    }
+
+
+    /**
+     * @param $weight
+     * 重量满减确认
+     */
+    private function weightCheckDiscount($weight)
+    {
+        $allrule = Source_Salerule_FavoutableRule::all();
+    }
+
+    /**
+     * @param $money
+     * 总价满价确认
+     */
+    private function priceCheckDiscount($money){
+
+    }
+
+    /**
+     * @param $couponCode 折扣郑号码
+     * 折扣卷的确认
+     */
+    public function couponCheck($couponCode){
+
     }
 
 
